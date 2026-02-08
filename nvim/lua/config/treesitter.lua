38b1f201282be3c03d766ff6770740713d68b843
Anders Høgden <ahoegden@icloud.com>
2026-02-08 15:00:54 +0100

    add more treesitter parsers

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,7 +1,24 @@
 local ts = require("nvim-treesitter.configs")
 
 ts.setup({
-	ensure_installed = { "c", "cpp", "lua", "vimdoc", "python" },
+	ensure_installed = {
+		"c",
+		"cpp",
+		"lua",
+		"vimdoc",
+		"python",
+		"cmake",
+		"make",
+		"bash",
+		"json",
+		"yaml",
+		"markdown",
+		"markdown_inline",
+		"diff",
+		"comment",
+		"regex",
+	},
 	highlight = { enable = true },
 	indent = { enable = true },
+	auto_install = true,
 })
