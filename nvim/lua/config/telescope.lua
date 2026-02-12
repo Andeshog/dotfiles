16cac88ac845876e88b1278097e61dff5c519289
Anders Høgden <anders.hogden@zeabuz.com>
2026-02-12 15:14:44 +0100

    enhance telescope with buffer deleter and ability to change directory during live grep

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,6 +1,31 @@
 -- ~/.config/nvim/lua/config/telescope.lua
 local M = {}
 
+local function grep_in_directory(prompt_bufnr)
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+	local current_input = action_state.get_current_line()
+	actions.close(prompt_bufnr)
+
+	require("telescope").extensions.directory.directory({
+		attach_mappings = function(dir_prompt_bufnr)
+			actions.select_default:replace(function()
+				local entry = action_state.get_selected_entry()
+				actions.close(dir_prompt_bufnr)
+				if entry then
+					require("telescope.builtin").live_grep({
+						search_dirs = { entry.value or entry[1] },
+					})
+					vim.defer_fn(function()
+						vim.api.nvim_feedkeys(current_input, "i", false)
+					end, 50)
+				end
+			end)
+			return true
+		end,
+	})
+end
+
 function M.setup()
 	local telescope = require("telescope")
 	local actions = require("telescope.actions")
@@ -14,6 +39,30 @@ function M.setup()
 				},
 				n = {
 					["<Esc>"] = actions.close,
+				},
+			},
+		},
+
+		pickers = {
+			live_grep = {
+				mappings = {
+					i = {
+						["<C-f>"] = grep_in_directory,
+					},
+					n = {
+						["<C-f>"] = grep_in_directory,
+					},
+				},
+			},
+			buffers = {
+				sort_mru = true,
+				mappings = {
+					i = {
+						["<C-d>"] = actions.delete_buffer,
+					},
+					n = {
+						["d"] = actions.delete_buffer,
+					},
 				},
 			},
 		},
