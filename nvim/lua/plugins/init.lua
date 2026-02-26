24eb881bee4ac288fa825575efe4488816d2b445
Anders Høgden <anders.hogden@zeabuz.com>
2026-02-26 08:34:32 +0100

    add harpoon plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -301,6 +301,50 @@ return {
 		},
 	},
 
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon:setup()
+
+			vim.keymap.set("n", "<leader>h", "<nop>", { desc = "Harpoon" })
+			-- Add file to harpoon
+			vim.keymap.set("n", "<leader>ha", function()
+				harpoon:list():add()
+			end, { desc = " add file" })
+
+			-- Toggle quick menu
+			vim.keymap.set("n", "<leader>he", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end, { desc = " quick menu" })
+
+			-- Navigate to files 1-4
+			vim.keymap.set("n", "<leader>hh", function()
+				harpoon:list():select(1)
+			end, { desc = "select file 1" })
+			vim.keymap.set("n", "<leader>hj", function()
+				harpoon:list():select(2)
+			end, { desc = "select file 2" })
+			vim.keymap.set("n", "<leader>hk", function()
+				harpoon:list():select(3)
+			end, { desc = "select file 3" })
+			vim.keymap.set("n", "<leader>hl", function()
+				harpoon:list():select(4)
+			end, { desc = "select file 4" })
+
+			-- Cycle through files
+			-- TODO: set these
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+		end,
+	},
+
 	----------------------------------------------------------
 	--------------- Editing & Text Manipulation --------------
 	----------------------------------------------------------
