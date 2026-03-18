0976ac95221f484c55fd9ab3f90ff9b9e74d2699
Anders Høgden <anders.hogden@zeabuz.com>
2026-03-18 08:14:44 +0100

    simplify statuscol

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -37,22 +37,21 @@ return {
 	{
 		"luukvbaal/statuscol.nvim",
 		config = function()
-			-- local builtin = require("statuscol.builtin")
+			local builtin = require("statuscol.builtin")
 			require("statuscol").setup({
-				-- configuration goes here, for example:
-				-- relculright = true,
-				-- segments = {
-				--   { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-				--   {
-				--     sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-				--     click = "v:lua.ScSa"
-				--   },
-				--   { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-				--   {
-				--     sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-				--     click = "v:lua.ScSa"
-				--   },
-				-- }
+				relculright = true,
+				segments = {
+					-- Git signs (gitsigns) in a separate column
+					{
+						sign = { namespace = { "gitsigns" }, maxwidth = 1, colwidth = 1 },
+						click = "v:lua.ScSa",
+					},
+					-- Line numbers
+					{
+						text = { builtin.lnumfunc, " " },
+						click = "v:lua.ScLa",
+					},
+				},
 			})
 		end,
 	},
