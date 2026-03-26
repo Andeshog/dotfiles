efccd196fba2e0686ac3bc3944588b7141408066
Anders Høgden <ahoegden@icloud.com>
2026-03-26 23:05:22 +0100

    add mouse mapping and theme to vim-visual-multi

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -296,6 +296,10 @@ return {
 	{
 		"mg979/vim-visual-multi",
 		branch = "master",
+		init = function()
+			vim.g.VM_mouse_mappings = 1
+			vim.g.VM_theme = "iceblue"
+		end,
 	},
 
 	"famiu/bufdelete.nvim",
